4fd1e46c67ef35df6e4a98fce7f505fa2a4d3695
Anusha32 <anushabr3@gmai.com>
2023-02-22 23:24:48 +0530

    changes in misaligned

--- misaligned/misaligned.js
+++ misaligned/misaligned.js
@@ -3,7 +3,7 @@ const {expect} = require('chai')
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i <= majorColors.length; i++) {
+    for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
         }
--- misaligned/misalignedTest.js
+++ misaligned/misalignedTest.js
@@ -3,4 +3,3 @@ let res = require('./misaligned');
 
 result = res.print_color_map();
 expect(result).equals(25);
-console.log('All is well (maybe!)');
