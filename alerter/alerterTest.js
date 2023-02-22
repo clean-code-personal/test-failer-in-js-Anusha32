9c133905226897e771191df7f31355feda1e6d69
Anusha32 <anushabr3@gmai.com>
2023-02-22 23:15:59 +0530

    update in alert

--- alerter/alerterTest.js
+++ alerter/alerterTest.js
@@ -5,3 +5,4 @@ alert.alertInCelcius(400.5);
 alert.alertInCelcius(303.6);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
+console.assert(alertFailureCount);
