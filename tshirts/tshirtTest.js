a452476fdc8d2343a6867ed3696c7c16690f336d
Anusha32 <anushabr3@gmai.com>
2023-02-23 14:34:11 +0530

    change in thshirt

--- tshirts/tshirtTest.js
+++ tshirts/tshirtTest.js
@@ -6,4 +6,4 @@ let s = require('./tshirts') ;
 console.assert(expect(s.size(37)).equals('S'));
 expect(s.size(40)).equals('M');
 expect(s.size(43)).equals('L');
-console.assert(expect(s.size(38)).equals('M')); 
+console.assert(expect(s.size(38)).equals('S')); 
