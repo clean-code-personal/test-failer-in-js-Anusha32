59ba56e0efee2a0d449a8d19e44d41440d5c0ea5
Anusha32 <anushabr3@gmai.com>
2023-02-26 01:21:29 +0530

    updated alerter

--- alerter/alerterTest.js
+++
@@ -1,7 +0,0 @@
-let alert = require('./alerter');
-
-alert.alertInCelcius(400.5);
-alert.alertInCelcius(303.6);
-console.log(`${alertFailureCount} alerts failed.`);
-console.log('All is well (maybe!)');
-console.assert(alertFailureCount);
