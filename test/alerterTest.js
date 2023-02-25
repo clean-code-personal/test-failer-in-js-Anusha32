53e87ae558a5fecd80e6582b605f17572c874688
Anusha32 <anushabr3@gmai.com>
2023-02-26 01:43:26 +0530

    alert failure updated

--- test/alerterTest.js
+++ test/alerterTest.js
@@ -1,7 +1,18 @@
-let alert = require('../alerter/alerter');
+// let alert = require('../alerter/alerter');
+// let 
+// alert.alertInCelcius(400.5);
+// alert.alertInCelcius(303.6);
+// console.log(`${alertFailureCount} alerts failed.`);
+// //console.assert(alertFailureCount);
 
-alert.alertInCelcius(400.5);
-alert.alertInCelcius(303.6);
-console.log(`${alertFailureCount} alerts failed.`);
-console.log('All is well (maybe!)');
-console.assert(alertFailureCount);
+let alertFailureCount = 0;
+const { assert } = require('chai');
+let alertInCelcius = require('../alerter/alerter');
+
+describe('Tests for alterer', function(){
+    it(function(){
+        alertFailureCount = alertInCelcius(400.5);
+        alertFailureCount = alertInCelcius(303.6);
+        assert.equal(alertFailureCount,2);
+    })
+})
