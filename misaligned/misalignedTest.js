32bc80e69473048e0a998e833ed8cbce09007b83
Anusha32 <anushabr3@gmai.com>
2023-02-26 14:22:58 +0530

    Updated tshirtTest

--- misaligned/misalignedTest.js
+++ misaligned/misalignedTest.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-let res = require('../misaligned/misaligned');
+let res = require('./misaligned');
 
 result = res.print_color_map();
 expect(result).equals(25);
--- test/tshirtTest.js
+++ test/tshirtTest.js
@@ -1,20 +1,20 @@
 const {expect, assert} = require('chai');
 const { it } = require('mocha');
 
-let size = require('../tshirts/tshirts') ;
+let s = require('../tshirts/tshirts') ;
 
 //Test cases using Mocha
 describe('User tshirt size test', function(){
   it('should return "S" when the value is 37', function(){
-    assert.equal(size(37),'S');
+    assert.equal(s.size(37),'S');
   });
   it('should return "M" when the size is 41', function(){
-    expect.equal(size(41),'M');
+    assert.equal(s.size(41),'M');
   });
   it('should return "L" when the size is 43', function(){
-    expect(size(43)).equals('L');
+    expect(s.size(43)).equals('L');
   });
   it('should return "S" when the value is 38',function(){
-    assert.equal(size(38),'S');
+    assert.equal(s.size(38),'S');
   });
 });
