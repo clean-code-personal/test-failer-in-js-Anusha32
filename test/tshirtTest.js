43966413e201103bba5b96c0ff2b72977aaa5346
Anusha32 <anushabr3@gmai.com>
2023-02-26 14:13:49 +0530

    Checks whether the test are running or not

--- test/tshirtTest.js
+++ test/tshirtTest.js
@@ -1,20 +1,20 @@
 const {expect, assert} = require('chai');
 const { it } = require('mocha');
 
-let s = require('../tshirts/tshirts') ;
+let size = require('../tshirts/tshirts') ;
 
 //Test cases using Mocha
 describe('User tshirt size test', function(){
   it('should return "S" when the value is 37', function(){
-    assert.equal(s.size(37),'S');
+    assert.equal(size(37),'S');
   });
-  it('should return "M" when the size is 40', function(){
-    expect.equal(s.size(41),'M');
+  it('should return "M" when the size is 41', function(){
+    expect.equal(size(41),'M');
   });
   it('should return "L" when the size is 43', function(){
-    expect(s.size(43)).equals('L');
+    expect(size(43)).equals('L');
   });
   it('should return "S" when the value is 38',function(){
-    assert.equal(s.size(38),'S');
-  })
-})
+    assert.equal(size(38),'S');
+  });
+});
