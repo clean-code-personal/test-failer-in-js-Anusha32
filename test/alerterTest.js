12f717ee4be55fb9aa4a88ebfae0730d45fac4c6
Anusha32 <anushabr3@gmai.com>
2023-02-26 14:45:13 +0530

    updated workflow

--- test/alerterTest.js
+++ test/alerterTest.js
@@ -1,9 +1,9 @@
 let alertFailureCount = 0;
 const { assert } = require('chai');
-let alertInCelcius = require('../alerter/alerter');
+const alertInCelcius = require('../alerter/alerter');
 
 describe('Tests for alterer', function(){
-    it(function(){
+    it('alert failure count',function(){
         alertFailureCount = alertInCelcius(400.5);
         alertFailureCount = alertInCelcius(303.6);
         assert.equal(alertFailureCount,2);
